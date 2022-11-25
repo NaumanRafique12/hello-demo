456a1047d55cb0fd0507b08be5c92431eaf8e178
Noman Rafique <108799714+NaumanRafique12@users.noreply.github.com>
2022-11-25 21:00:48 +0500

    Update app.py

--- Demo_Hello/app.py
+++ Demo_Hello/app.py
@@ -3,6 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
